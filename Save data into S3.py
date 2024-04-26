0ee120943e1c95e7689bee946856d3011dd37287
Simon Murua <Simon.Murua@emsbilling.com>
2024-04-25 21:13:09 -0300

    branch1

---
+++ Save data into S3.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import boto3
+
+def GetDataApi():
+    response_API = requests.get('https://api.covid19india.org/state_district_wise.json')
+    data = response_API.json()  # Parse JSON directly into a dictionary
+    return data
+
+def save_to_s3(data, bucket_name, object_key, aws_access_key_id, aws_secret_access_key):
+    # Initialize S3 client with provided credentials
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    # Convert data to JSON string
+    json_data = json.dumps(data)
+
+    # Upload data to S3 bucket
+    s3.put_object(Bucket=bucket_name, Key=object_key, Body=json_data)
+
+# Main function to fetch data and save to S3
+def main():
+    data = GetDataApi()
+    bucket_name = 'datafrompython'  # Replace with your S3 bucket name
+    object_key = 'covid_data.json'  # Specify the key for the S3 object
+    aws_access_key_id = 'AKIAVYRKGDUOUZLMF4KO'  # Replace with your AWS access key ID
+    aws_secret_access_key = 'k5f6DK9fm1Yve754yLeM10k8EDPILSeQjizbOhJU'  # Replace with your AWS secret access key
+    save_to_s3(data, bucket_name, object_key, aws_access_key_id, aws_secret_access_key)
+
+if __name__ == "__main__":
+    main()
